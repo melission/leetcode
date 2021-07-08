525f590520f2c641ce03005b450dda002266430f
PolinBelousova <melission18@gmail.com>
2021-07-08 16:32:27 +0300

    08/07/2021 [second solution]

--- Chellenges/2021/July/Max length of repeated subarrey.py
+++ Chellenges/2021/July/Max length of repeated subarrey.py
@@ -1,10 +1,8 @@
 # https://www.geeksforgeeks.org/longest-common-subarray-in-the-given-two-arrays/ first sol
 
 def findLength(nums1, nums2) -> int:
-    common_length = 0
     a1 = len(nums1)
     a2 = len(nums2)
-
     dp = [[0 for i in range(a1+1)] for i in range(a2+1)]
     for i in range(a1 -1, -1, -1):
         for j in range(a2 -1, -1, -1):
@@ -12,13 +10,23 @@ def findLength(nums1, nums2) -> int:
             if nums1[i] == nums2[j]:
                 dp[j][i] = dp[j+1][i+1] + 1
 
-    for i in dp:
-        for j in i:
-            common_length = max(common_length, j)
-    return common_length
+    # for i in dp:
+    #     for j in i:
+    #         common_length = max(common_length, j)
+    return max(max(row) for row in dp)
+
+
+def findLength2(nums1, nums2) -> int:
+    a1, a2 = len(nums1), len(nums2)
+    dp = [[0] * (a1+1) for x in range(a2+1)]
+    for i in range(a1 -1, -1, -1):
+        for j in range(a2 -1, -1, -1):
+            if nums1[i] == nums2[j]:
+                dp[j][i] = dp[j+1][i+1] + 1
+    return max(max(row) for row in dp)
 
 
 if __name__ == '__main__':
-    print(findLength(
-        nums1=[1, 2, 3, 2, 1], nums2=[3, 2, 1, 4, 7]
+    print(findLength2(
+        nums1=[1,2,3,2,1], nums2=[3,2,1,4]
     ))
