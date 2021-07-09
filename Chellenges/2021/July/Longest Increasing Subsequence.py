16731ed60c09584efa59aa9a06070b5ffd473176
PolinBelousova <melission18@gmail.com>
2021-07-09 13:23:14 +0300

    09/07/2021 add a link. add a print func

--- Chellenges/2021/July/Longest Increasing Subsequence.py
+++ Chellenges/2021/July/Longest Increasing Subsequence.py
@@ -1,3 +1,4 @@
+# https://leetcode.com/explore/challenge/card/july-leetcoding-challenge-2021/609/week-2-july-8th-july-14th/3808/
 def lengthOfLIS(nums) -> int:
     """should be O(n**2)"""
     array_size = len(nums)
@@ -5,14 +6,15 @@ def lengthOfLIS(nums) -> int:
 
     for i in range(1, array_size):
         for j in range(0, i):
-            print(f' i {i}, j {j}; li {li}')
+            print(f"nums[i] {nums[i]}, nums[j] {nums[j]};\n "
+                  f"li[i] {li[i]}, li[j] {li[j]}, and li[j]+1 {li[j]+1}\n "
+                  f"full nums list {nums}, li list {li}\n")
             if nums[i] > nums[j] and li[i] < li[j]+1:
                 li[i] = li[j]+1
     return max(li)
 
 
-
 if __name__ == '__main__':
     print(lengthOfLIS(
-        [0,1,0,3,2,3]
+        [7,7,7,7,7,7,7]
     ))
